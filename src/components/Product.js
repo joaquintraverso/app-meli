be86c5ad3ff4b5d76b8a2b9e96af154b36699e25
joaquintravero <joaquintraverso03@gmail.com>
2022-09-05 19:37:31 -0300

    src

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,7 +10,7 @@ export default function Product(props) {
   const img = `https://http2.mlstatic.com/D_${img_id}-O.jpg`
 
   return (
-    <Card style={{ width: '18rem'}}>
+    <Card style={{ width: '18rem', border:'1px solid black'}}>
       <Card.Img className='card-img' variant="top" src={img} />
       <Card.Body>
         <Card.Title>{product.title}</Card.Title>
